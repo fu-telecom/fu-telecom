34d0d0fe1e7f3704fe09b4c9aefc1cc97f39a92d
Ben V <veliebm@gmail.com>
2024-06-16 22:47:18 -0500

    refactor: make phone-processing.php more readable
    JUSTIFICATION:
    phone-processing.php is hard to understand. We should make it easier to
    understand.

--- fuconfig/phone-processing.php
+++ fuconfig/phone-processing.php
@@ -4,49 +4,42 @@ include_once ('FUConfig.php');
 
 $resultProcessor = new ProcessResult(false);
 
-$directoryProcessor = new DirectoryProcessor();
-$extensionsProcessor = new ExtensionsProcessor();
-$hintsProcessor = new HintsProcessor();
-$buttonsProcessor = new ButtonsProcessor();
-
 // Update Asterisk Realtime database
 $processor = new PhoneProcessor($resultProcessor);
 $processor->ProcessAllPhones();
 
 // Write files
+$directoryProcessor = new DirectoryProcessor();
 $resultProcessor->AddResult($directoryProcessor->ProcessDirectories());
+$extensionsProcessor = new ExtensionsProcessor();
 $resultProcessor->AddResult($extensionsProcessor->ProcessExtensions());
+$hintsProcessor = new HintsProcessor();
 $resultProcessor->AddResult($hintsProcessor->ProcessHints());
+$buttonsProcessor = new ButtonsProcessor();
 $resultProcessor->AddResult($buttonsProcessor->ProcessButtons());
 
 $resultXml = $resultProcessor->GetXmlResult();
 
 $xpathToError = "/xml/PhoneProcessorResult/error";
-$xpathToTime = "/xml/TimeStamp";
-
 $error = $resultXml->xpath($xpathToError)[0];
-$timestamp = $resultXml->xpath($xpathToTime)[0];
 
 if ($error != null) {
   if ($error == 1) {
-
-    //An error occurred. Write the file somewhere.
+    // An error occurred. Write the file somewhere.
+    $xpathToTime = "/xml/TimeStamp";
+    $timestamp = $resultXml->xpath($xpathToTime)[0];
     $fileDestination = "/asterisk_scripts/error_log/" . $timestamp . ".log";
     $resultProcessor->LogText("Writing Error Log:" . $fileDestination . "<br>");
 
-    //Prepare it as a usable document.
+    // Prepare it as a usable document.
     $dom = new DOMDocument('1.0');
     $dom->preserveWhiteSpace = false;
     $dom->formatOutput = true;
     $dom->loadXml($resultXml->asXML());
     $dom->save($fileDestination);
   }
-
 } else {
   throw new Exception("Can't find error xml in result.");
 }
 
 OutputXML($resultXml);
-
-
-?>
